89140e5aee3d8a6a0189814dc672b5283ff32bbf
Thabo Mokoenaa <thabo.mokoenaa99@gmail.com>
2026-09-06 08:51:27 -0300

    Add Google Tag Manager GTM-TXDP6P7T to the document head and body.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,27 @@ export default function RootLayout({
       lang="pt-BR"
       className={`${poppins.variable} ${playfair.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TXDP6P7T');`,
+          }}
+        />
+      </head>
       <body className="min-h-full font-sans">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TXDP6P7T"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         <Script id="tiktok-pixel" strategy="afterInteractive">
           {`
 !function (w, d, t) {
